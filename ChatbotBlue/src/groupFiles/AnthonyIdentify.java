545ed1084fb99dbb51f25f7962c76f1ef8d25a0f
Student 8 <Student 8@10.8.33.170>
2016-09-29 13:57:26 -0400

    Testing Phase

--- ChatbotBlue/src/groupFiles/AnthonyIdentify.java
+++ ChatbotBlue/src/groupFiles/AnthonyIdentify.java
@@ -1,5 +1,48 @@
 package groupFiles;
 
+import java.util.Scanner;
+
 public class AnthonyIdentify {
-//test test
+	static Scanner input;
+	static String user;
+
+	public static void main(String[] args) {
+		//demonstrateStringMethods();
+		createFields();
+		promptName();
+		promptForever();
+		}
+	
+	public static void promptName() {
+		print("enter your name");
+		user = input.nextLine();
+		print("Glad to meet you, "+user + "."
+				+"\nFor the rest of the time," 
+				+"\nI will call you "+user
+				+".\nYou may call me Computer."
+				+"\nWe should become friends." );
+	}
+	
+	public static void promptForever(){
+		while(true){
+			promptInput();
+		}
+	}
+	
+	public static void promptInput(){
+		print("Please type something "+user+".");
+		String userInput = input.nextLine();
+		print("Congratulations!You typed: "+userInput);
+	}
+	
+	public static void createFields(){
+		input = new Scanner(System.in);
+		user = "";
+	}
+	
+	public static void print(String s){
+		System.out.println(s);
+		
+	}
+	
 }
