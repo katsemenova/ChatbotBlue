925b64ef72ccd4ce39c934eeddb91d1545b71d13
Student 8 <Student 8@10.8.33.159>
2016-09-30 13:35:11 -0400

    Created talk

--- ChatbotBlue/src/groupFiles/JenniberJokes.java
+++ ChatbotBlue/src/groupFiles/JenniberJokes.java
@@ -1,5 +1,55 @@
 package groupFiles;
 
-public class JenniberJokes {
+public class JenniberJokes implements Chatbot{
+	private String jokeResponse;
+	private boolean inJokeMode;
+	
+	private int jokeCount;
+	
+	private String[] jokesQuestions = {"What do ghosts eat for supper?","How did the glamorous ghoul earn her living?","How did the ghost patch his sheet?"};
+	private String[] jokesAnswers = {"Spooketi","She was a cover ghoul.","With a pumpkin patch."};
+	
+	public JenniberJokes(){
+		
+	}
+	
+	public void talk(){
+		inJokeMode = true;
+		while(inJokeMode){
+			printResponse(); //helper method
+			jokeResponse = Main.promptInput();
+			//negate use !
+			if(!isTriggered(jokeResponse)){
+				inJokeMode = false;
+				Main.promptForever();
+			}
+		}
+	}
 
+	
+	public boolean isTriggered(String userInput) {
+		if(Main.findKeyword(userInput, "ghosts", 0)>=0){
+			return true;
+		}
+		if(Main.findKeyword(userInput, "ghoul", 0)>=0){
+			return true;
+		}
+		if(Main.findKeyword(userInput, "ghost", 0)>=0){
+			return true;
+		}
+			
+		return false;
+	}
+	
+	private void printResponse() {
+		if(helloCount>4){
+			int responseSelection = (int)(Math.random()*jokesQuestions.length);
+			
+			JenniberMain.print(jokeResponse[responseSelection]);
+		}else{
+			int responseSelection = (int)(Math.random()*calmResponses.length);
+			JenniberMain.print(calmResponses[responseSelection]);
+		}
+		
+	}
 }
