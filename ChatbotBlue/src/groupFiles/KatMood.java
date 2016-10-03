7dcf1666b6ac56d39f599ecbc611e4792614d986
Kat Semenova <katya.semenova1405@gmail.com>
2016-10-02 23:56:51 -0400

    updates

--- ChatbotBlue/src/groupFiles/KatMood.java
+++ ChatbotBlue/src/groupFiles/KatMood.java
@@ -2,11 +2,31 @@ package groupFiles;
 
 public class KatMood implements Chatbot{
 
-	@Override
+	public String[] happyWords={"smart", "genious","beautiful","cute","pretty","amazing","adorable","funny","lovely","cute"};
+	public String[] sadWords={"ugly","dumb","stupid","crazy","bad","disgusting","gross","annoying"};
+	public int sadLevel;
+	public int happyLevel;
+	public int complimentCount;
+	
+	public void KatHello(){
+		sadLevel=0;
+		happyLevel=0;
+		complimentCount=0;
+	}
 	public void talk() {
-		// TODO Auto-generated method stub
 		
 	}
+//	private static boolean addressingChatbot(String searchString, int psn) {
+//		String[] youArray={"you ","you're ","you are ","you are very ","you are really "};
+//		for(int i=0;i<youArray.length;i++){
+	//		testWord=youArray[i];
+	//		if(psn-testWord.length>=0&&searchString.substring(psn-testWord.length,psn).equals(testWord)){
+	//			return true;
+	//		}
+//		}
+//		return false;	
+//	}
+	
 
 	@Override
 	public boolean isTriggered(String userInput) {
--- ChatbotBlue/src/groupFiles/Main.java
+++ ChatbotBlue/src/groupFiles/Main.java
@@ -48,6 +48,10 @@ public class Main {
 				inMainLoop=false;
 				jenniber.talk();
 			}
+			else if(kat.isTriggered(response)){
+				inMainLoop=false;
+				kat.talk();
+			}
 			else 
 				print("I don't understand");
 		}
@@ -78,7 +82,7 @@ public class Main {
 			if(psn+keyword.length()<searchString.length()){
 				after=searchString.substring(psn+keyword.length(),psn+keyword.length()+1);
 			}
-			if((before.compareTo("a")<0) && (after.compareTo("a")<0)&&noNegations(searchString,psn)){				noNegations(searchString,psn);
+			if((before.compareTo("a")<0) && (after.compareTo("a")<0)&&noNegations(searchString,psn)){noNegations(searchString,psn);
 				return psn;
 			}
 			else
@@ -132,6 +136,7 @@ public class Main {
 		tamanna = new TamannaAnnoy();
 		jenniber = new JenniberJokes();
 		anthony = new AnthonyIdentify();
+		kat= new KatMood();
 	}
 	
 	public static void demonstrateStringMethods(){
@@ -157,34 +162,28 @@ public class Main {
 		 //this while loop last as long as there are words left in the original String
 		 while(s.length() > 0){
 
-		 String currentCut = "";
-		 String nextWord = "";
+			 String currentCut = "";
+			 String nextWord = "";
 
-		 //while the current cut is still less than the line length 
-		 //AND there are still words left to add
-		 while(currentCut.length()+nextWord.length() < cutoff && s.length() > 0){
+			 //while the current cut is still less than the line length 
+			 //AND there are still words left to add
+			 while(currentCut.length()+nextWord.length() < cutoff && s.length() > 0){
+				 //add the next word
+				 currentCut += nextWord;
+				 //remove the word that was added from the original String
+				 s = s.substring(nextWord.length());
+				 //identify the following word, exclude the space
+				 int endOfWord = s.indexOf(" ");
+				 //if there are no more spaces, this is the last word, so add the whole thing
+				 if(endOfWord == -1) {
+					 endOfWord = s.length()-1;//subtract 1 because index of last letter is one les than length
+				 }
+				 //the next word should include the space
+				 nextWord = s.substring(0,endOfWord+1);
+			 }
 
-		 //add the next word
-		 currentCut += nextWord;
-		 
-		 //remove the word that was added from the original String
-		 s = s.substring(nextWord.length());
-		 
-		 //identify the following word, exclude the space
-		 int endOfWord = s.indexOf(" ");
-
-		 //if there are no more spaces, this is the last word, so add the whole thing
-		 if(endOfWord == -1) {
-		 endOfWord = s.length()-1;//subtract 1 because index of last letter is one les than length
-		 }
-
-		 //the next word should include the space
-		 nextWord = s.substring(0,endOfWord+1);
-		 }
-
-		 printString +=currentCut+"\n";
-
-		 }
-		 System.out.print(printString);
-		 }
+			 printString +=currentCut+"\n";
+		 	}
+		 	System.out.print(printString);
 	}
+}
