85f9d1d0d7e9def5719683e8265e197f22df07fe
USER <USER@TS-131-018W8PRO>
2016-10-03 21:53:14 -0400

    Prototype/2

--- ChatbotBlue/src/groupFiles/AnthonyIdentify.java
+++ ChatbotBlue/src/groupFiles/AnthonyIdentify.java
@@ -3,9 +3,11 @@ package groupFiles;
 public class AnthonyIdentify implements Chatbot{
 	
 	private boolean inLoop;
-	int topicSwitch = 0;//trigger for changeOfSubject
-	private String topic;//records sentence
-	private String subject;//records keyword
+	int topicSwitch = 0;//used to trigger the isTriggered function
+	private String sent;//records future sentences to be compared against key 
+	private String key;//records 1st sentence for future comparisons
+	private String subject;//recorded keyword
+	private int reminder = 0;//checks if it has been triggered
 	
 	private String[] changeOfSubject =
 		{"You've been talking a lot about" +
@@ -18,9 +20,10 @@ public class AnthonyIdentify implements Chatbot{
 	
 	@Override
 	public void talk() {
-		inLoopA = true;
-		while(inLoopA){
+		inLoop = true;
+		while(inLoop){
 			Main.print("(Math.random()*changeOfSubject.length) + 1");
+			reminder = false;
 			//static call on promptInput method from 
 			//Main class 
 				Main.promptForever();
@@ -31,12 +34,30 @@ public class AnthonyIdentify implements Chatbot{
 	@Override
 	public boolean isTriggered(String userInput) {
 			{
-			String topic = userinput;
-			String[] array2 = topic.split(" ");
-		}
-		return false;	
+			String sent = userinput;
+			String[] sentence = sent.split(" ");
+			
+			if (reminder = 0)//reminder will be used to see if the keyword has been recorded or not,
+				String key = sentence;// and whether or not a sentence has been recorded or not to be used in comparisons 
+				reminder = 1;
+			
+			if (remainder = 1)
+				for (f = 0; f < sent.length; f++){
+					for(d = 0; d < key.length; d++){
+						if (key[d] = sentence[f])
+							subject = sentence[f];//when a match has been found, reminder wil be upped to 2 and simplify the process.
+							reminder = 2;
+							topicSwitch = 1;
+					}	
+				}
+			if(remainder = 2)
+				for(d = 0; d < key.length; d++){
+					if (key[d] = sentence[f])
+						subject = sentence[f];
+						reminder = 2;
+			}
+		return true;	
 	}
-	
-	public void Checking(String)
+
 }
 
