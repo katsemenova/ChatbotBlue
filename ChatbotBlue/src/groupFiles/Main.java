5bde9f45c036169c766c0c1ecd2946a3886cd392
Hussain <Hussain@Hussain-PC.home>
2016-10-04 20:35:03 -0400

    new

--- ChatbotBlue/src/groupFiles/Main.java
+++ ChatbotBlue/src/groupFiles/Main.java
@@ -135,7 +135,7 @@ public class Main {
 		
 		tamanna = new TamannaAnnoy();
 		jenniber = new JenniberJokes();
-		anthony = new AnthonyIdentify();
+		//anthony = new AnthonyIdentify();
 		kat= new KatMood();
 	}
 	
