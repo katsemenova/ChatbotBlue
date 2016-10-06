f60c5c7401e8189b23cdf4d0057662638a94a40f
Student 8 <Student 8@10.8.33.170>
2016-10-06 13:59:54 -0400

    Fix

--- ChatbotBlue/src/groupFiles/AnthonyIdentify.java
+++ ChatbotBlue/src/groupFiles/AnthonyIdentify.java
@@ -34,58 +34,57 @@ public class AnthonyIdentify implements Chatbot{
 	public boolean isTriggered(String userInput) 
 	{
 			
-			String sent = userInput;
-			String[] sentence = sent.split(" ");//sentence is an array containing each word of the user's sentence as their own item.
-			
-			if (triggerCount == 0)
-			{	Main.print("stage1");					//triggerCount will be used to see if the keyword has been recorded or not,
-				checker = sentence; // and whether or not a sentence has been recorded or not to be used in comparisons 
-				triggerCount += 1;
-				return false;
-			}
-			//wooooork
-			if (triggerCount == 1){
-				Main.print("stage 2");
-			System.out.println(triggerCount);
-			System.out.println(keyword);
-			for (int f = 0; f < sentence.length; f++)
-				{
-					for(int d = 0; d < checker.length;d++)
-					{
-						if (checker[d] == sentence[f])
-						{
-							keyword = sentence[f];//when a match has been found, the keyword is recorded to simplify the rest.
-							triggerCount += 1;
-						}	
-					}	
-				}
+		String sent = userInput;
+		String[] sentence = sent.split(" ");//sentence is an array containing each word of the user's sentence as their own item.
+		
+		if (triggerCount == 0)
+		{	Main.print("stage1");					//triggerCount will be used to see if the keyword has been recorded or not,
+			checker = sentence; // and whether or not a sentence has been recorded or not to be used in comparisons 
+			triggerCount += 1;
 			return false;
-			}
-			
-			if(triggerCount == 2)
-			{Main.print("stage 3");
-				for (int s = 0; s < sent.length(); s++)
-				{
-					keyword = sentence[s];//The keyword is then used to simplify the function
-					triggerCount += 1;
-				}
-			return false;
-			}
-							
-			if (triggerCount == 4)
-			{Main.print("stage 4");
-				triggerCount = 0;
-				return true;
-			}
-							
-			else
+		}
+		//wooooork
+		else if (triggerCount == 1){
+			Main.print("stage 2");
+		System.out.println(triggerCount);
+		System.out.println(keyword);
+		for (int f = 0; f < sentence.length; f++)
 			{
-				Main.print(changeOfSubject[(int) ((Math.random()*3) + 1)]);
-				triggerCount = 0;//if nothing matches in the next sentence,the function will reset.
-				return false;
-			}	
+				for(int d = 0; d < checker.length;d++)
+				{
+					if (checker[d] == sentence[f])
+					{
+						keyword = sentence[f];//when a match has been found, the keyword is recorded to simplify the rest.
+						triggerCount += 1;
+					}	
+				}	
+			}
+		return false;
+		}
+		
+		else if(triggerCount == 2)
+			{
+			Main.print("stage 3");
+			for (int s = 0; s < sent.length(); s++)
+			{
+				keyword = sentence[s];//The keyword is then used to simplify the function
+				triggerCount += 1;
+			}
+		return false;
+		}
+							
+		else if (triggerCount == 4)
+		{
+			Main.print("stage 4");
+			triggerCount = 0;
+			return true;
+		}
+						
+		else
+		{
+			Main.print(changeOfSubject[(int) ((Math.random()*3) + 1)]);
+			triggerCount = 0;//if nothing matches in the next sentence,the function will reset.
+			return false;
+		}	
 	}
-
 }
-
-
