116b2293a596807be3cad581e9ddf920f449935c
jfranco7156 <jfranco7156@bths.edu>
2016-10-06 23:04:37 -0400

    Added comments

--- ChatbotBlue/src/groupFiles/JenniberJokes.java
+++ ChatbotBlue/src/groupFiles/JenniberJokes.java
@@ -6,20 +6,23 @@ public class JenniberJokes implements Chatbot{
 	private boolean inJokeMode;
 	private int jokeCount;
 	private int triggerNum;
-
-	private String[] userResponse={"yes", "yea","sure","ok", "okay"};
+	
+	//user responses to check over when a triggerWord is not used
+	private String[] userResponse={"yes", "yea","sure","ok","alright","yep","why not"};
 	
 	//Created 2D Arrays
+	//Jokes with its respective answer
 	private String[][] jokes = {
 			{"What do ghosts eat for supper?","Spooketi"},
 			{"How did the glamorous ghoul earn her living?","She was a cover ghoul"},
 			{"How did the ghost patch his sheet?","With a pumpkin patch"},
-			{"Why didn’t the skeleton want to go to school?","His heart wasn’t in it"},
-			{"Why didn’t the skeleton go to the ball?","Because he had no BODY to go with"},
+			{"Why didnâ€™t the skeleton want to go to school?","His heart wasnâ€™t in it"},
+			{"Why didnâ€™t the skeleton go to the ball?","Because he had no BODY to go with"},
 			{"What do you get when you cross a witch with sand?","A sandwich!"},
 			{"What kind of dessert does a ghost like?","I scream!"},
 			{"What do mummies like listening to on Halloween?","Wrap music!"}
 	};
+	//Possible triggerWords
 	private String[][] jokeTriggers = {
 			{"supper","dinner","meal","food"}, 
 			{"living","job","work"},
@@ -35,6 +38,7 @@ public class JenniberJokes implements Chatbot{
 		jokeCount = 0;
 	}
 	
+	//asks a joke to user and prompts a response from the user
 	public void talk(){
 		inJokeMode = true;
 		
@@ -46,6 +50,8 @@ public class JenniberJokes implements Chatbot{
 	}
 
 	public boolean isTriggered(String userInput) {
+		//Checks user's input to see if a trigger word was used to do a joke
+		//triggerNum is used for printResponse, to ask a joke in relation to the user's input
 		for(int x=0; x<jokeTriggers.length; x++){
 			for(int y=0; y<jokeTriggers[x].length; y++){
 				triggerString = jokeTriggers[x][y];
@@ -55,6 +61,9 @@ public class JenniberJokes implements Chatbot{
 				}
 			}	
 		}
+		//if user wants to hear a joke without having a  trigger word
+		//it will select a random number as the triggerNum
+		//empty triggerString will be used to check in printResponse and print the appropriate string
 		for(int z=0; z<userResponse.length; z++){
 			if(Main.findKeyword(userInput, userResponse[z], 0)>=0){
 				triggerNum = getTriggerNum();
@@ -66,6 +75,7 @@ public class JenniberJokes implements Chatbot{
 		return false;
 	}
 	
+	//generate a random triggerNum to be used when a trigger word was not used
 	private int getTriggerNum(){	
 		double rand = Math.random();
 		int trig = (int) (jokes.length*rand);
@@ -73,10 +83,12 @@ public class JenniberJokes implements Chatbot{
 	}
 	
 	private void printResponse(int triggerNum) {
-		//will exit while loop when user has guessed 3 times or answered the joke correctly
-		//else it will ask the joke
+		//converts all letters in the answer to the joke to lower case  
 		String jokeAnswer = jokes[triggerNum][1].toLowerCase();
 		
+		//will exit while loop, reset jokeCount, and change mood when user has guessed 3 times or answered the joke correctly
+		//else if there is no triggerWord and the user wants a joke, it will ask user a joke
+		//else it will ask the joke while also stating the rigger word that was used by the user
 		if(jokeAnswer.equals(jokeResponse)){
 			Main.print("That's right! The answer is: "+jokes[triggerNum][1]);
 			inJokeMode = false;
