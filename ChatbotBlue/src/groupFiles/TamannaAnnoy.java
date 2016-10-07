c535e0d65ca21e771e7eeecc708af64890dccfe9
Hussain <Hussain@Hussain-PC.home>
2016-10-06 23:54:53 -0400

    Editing a few lines

--- ChatbotBlue/src/groupFiles/TamannaAnnoy.java
+++ ChatbotBlue/src/groupFiles/TamannaAnnoy.java
@@ -45,6 +45,7 @@ public class TamannaAnnoy implements Chatbot{
 			responseAfter = userResponse;
 			
 			if(!isTriggered(userResponse)){
+				Main.print("Cool.");
 				inResponseLoop = false;
 				Main.promptForever();
 			}
